bd872036a60bc8b46d85ff852020cb7fa63edf13
faze20 <42577269+faze20@users.noreply.github.com>
2022-03-18 12:16:19 -0700

    uility2

--- src/components/Utilities.js
+++ src/components/Utilities.js
@@ -3,7 +3,7 @@ import TextTransition, { presets } from "react-text-transition";
 
 // import '../App.css'
 import '../Utilities.css';
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 function Utilities() {
@@ -39,21 +39,106 @@ function Utilities() {
 
             <div className="utility_subnav">
                 <ul className='utility_ul'>
-                 <li>SEO</li>
-                <li>Youtube</li>
-                <li>Email</li>
-                <li>Webtools</li>
-                <li>Discover</li>
-                <li>AI</li>
-                <li></li>
+                    <li>
+                        <Link to='#'>
+                        <p>SEO</p>
+                            
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/'>
+                        <p > Youtube</p>
+                           
+                        </Link>
+
+                    </li>
+                    <li>
+                        <Link href='#'>
+                            <p > Email</p>
+                           
+                        </Link>
+
+                    </li>
+                    <li>
+                        <Link href='#'>
+                        <p > Webtools</p>
+                           
+                        </Link>
+
+                    </li>
+                    <li>
+                        <Link href='#'>
+                        <p >Discover</p>
+                           
+                        </Link>
+
+                    </li>
+                    <li>
+                        <Link href='#'>
+                        <p > AI</p>
+                          
+                        </Link>
+                    </li>
                 </ul>
             </div>
         <div className='utility_pagecontent'  style= {{ background :"url('/assets/sdbnew.png') center center" } }>
            <div className='content_left'>
 
-                Utilities
+               <h1>
+                   Free SEO & Digital Marketing Tools
+               </h1>
+               <h2>
+                   The Best Free Seo and Digital Marketing Solution Your Business Will Ever Need.
+               </h2>
+               <button>Image optimiser</button>
            </div>
 
+        </div>
+        <div className="countdowns">
+            <div className="countdown_content">
+                <h4> 264M</h4>
+                <p>Emails verified</p>
+
+            </div>
+            <div className="countdown_content">
+            <h4> 705M</h4>
+                <p>Keyword Analyzed</p>
+
+            </div>
+            <div className="countdown_content">
+            <h4> 117K</h4>
+                <p>Happy Users</p>
+
+            </div>
+        </div>
+        <div className="seo_solutions">
+            <div className="seo_solcontents">
+                <h4>Keyword Research Tool</h4>
+            </div>
+            <div className="seo_solcontents">
+                <h4>Keyword Research Tool</h4>
+            </div>
+            <div className="seo_solcontents">
+                <h4>Keyword Research Tool</h4>
+            </div>
+            <div className="seo_solcontents">
+                <h4>Keyword Research Tool</h4>
+            </div>
+            <div className="seo_solcontents">
+                <h4>Keyword Research Tool</h4>
+            </div>
+            <div className="seo_solcontents">
+                <h4>Keyword Research Tool</h4>
+            </div>
+            <div className="seo_solcontents">
+                <h4>Keyword Research Tool</h4>
+            </div>
+            <div className="seo_solcontents">
+                <h4>Keyword Research Tool</h4>
+            </div>
+            <div className="seo_solcontents">
+                <h4>Keyword Research Tool</h4>
+            </div>
         </div>
         <div>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est fuga
